dc80e4256e31e0a477dde2053fbfdfb77d5e32dd
victordanielmun <31445435+victordanielmun@users.noreply.github.com>
2024-04-15 07:11:51 -0500

    Initial Commit

---
+++ components/popUp/popUp.js
@@ -0,0 +1,121 @@
+/**
+ * Template Name: PopUp - v1.0
+ * Updated: 2024-04-16
+ * Author: Lemonk Studio
+ */
+
+(function () {
+  "use strict";
+
+  // variable to validate if the popup has been shown in the browser
+  let viewNotification = false;
+
+  // variable for delay on mobile devices
+  let timeDelay = 2000;
+
+  // variable to validate if the resolution is maximum 600px (Mobile devices)
+  const isMobile = window.matchMedia(
+    "only screen and (max-width: 600px)"
+  ).matches;
+
+  // variable to validate if the resolution is maximum 960px (Tablet devices)
+  const isTablet = window.matchMedia(
+    "only screen and (min-width: 601px) and (max-width: 960px)"
+  ).matches;
+
+
+  /**
+   * Remove mouseOut event listener on screens larger than 960px and call PopUp.
+   */
+  function removeEventListener() {
+    viewNotification = true;
+    agregarPopup();
+    window.removeEventListener("mouseout", function () {});
+  }
+
+
+
+  /** Function that when loading the page validates if the resolution
+   *  is greater than 960px and adds the mouseout event to activate
+   * the function removeEventListene once if the mouse leaves the page
+   * or if the resolution is less than 960px call the PopUp once after a delay */
+
+  window.addEventListener("load", function () {
+    if (
+      viewNotification === false &&
+      isMobile === false &&
+      isTablet === false
+    ) {
+      setTimeout(function () {
+        window.addEventListener("mouseout", function (event) {
+          
+          event.preventDefault();
+          const y = event.pageY;
+          if (y < 5 && viewNotification === false) {
+            removeEventListener();
+          }
+        });
+      }, timeDelay);
+    } else {
+      setTimeout(function () {
+        agregarPopup();
+      }, timeDelay);
+    }
+  });
+})();
+
+function agregarPopup() {
+  
+// Create the <aside> element
+  let aside = document.createElement("aside");
+  aside.id = "popup";
+  aside.className = "popup-bg";
+
+  //Create the internal structure of the popup
+  aside.innerHTML = `
+    <div class="popup-body">
+      <div class="popup-content">
+        <div class="popup-exit-btn">
+          <span id="closePopup">X</span>
+        </div>
+        <div class="popup-row">
+          <div class="popup-image">
+            <div class="popup-img-container">
+              <img 
+                src="assets/img/popUp/girl in ipad 1.png" 
+                alt="popup-image" 
+              />
+              <div class="popup-img-banner">
+                <img
+                  src="assets/img/popUp/logoB.svg"
+                  alt="popup-image-banner"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="popup-text">
+            <div class="popup-text-container">
+              <hr>
+              <h3>3 MONTHS OF</h3>
+              <h2>Britannica Premium</h2>
+              <div id="popup-text-price">
+                <hr>
+                <h3>FOR $10</h3>
+                <hr>
+              </div>
+              <button class="popup-button">Subscribe!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+  // Add the popup to the end of the document body
+  document.body.appendChild(aside);
+
+  // Remove the aside when the close button is clicked
+  let closeBtn = document.getElementById("closePopup");
+    closeBtn.addEventListener("click", function() {
+        aside.remove(); 
+    });
+}
